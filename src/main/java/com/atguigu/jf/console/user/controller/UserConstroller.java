1b1aedef07ab70566b8b8dded34a56ec2f7c376b
liuji789 <583115762@qq.com>
2017-01-09 20:26:52 +0800

    1111

--- src/main/java/com/atguigu/jf/console/user/controller/UserConstroller.java
+++ src/main/java/com/atguigu/jf/console/user/controller/UserConstroller.java
@@ -35,21 +35,22 @@ import com.github.pagehelper.PageInfo;
 @Controller
 @RequestMapping("user")
 public class UserConstroller {
-	
+
 	private Logger logger = LoggerFactory.getLogger(UserConstroller.class);
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping("selecSysopList")
 	@ResponseBody
-	public PageModel<SysOp> selecSysopList(String search,String opKind,String opName,int start,int limit) throws Exception{
-		
+	public PageModel<SysOp> selecSysopList(String search, String opKind, String opName, int start, int limit)
+			throws Exception {
+
 		Map<String, Object> map = new HashMap<>();
 		map.put("limit", limit);
 		map.put("opName", opName);
 		map.put("opKind", opKind);
-		
+
 		if ("search".equals(search)) {
 			start = 0;
 		}
@@ -57,126 +58,122 @@ public class UserConstroller {
 		List<SysOp> list = userService.selectSysOpList(map);
 		PageModel<SysOp> pageModel = new PageModel<>();
 		Integer total = userService.selectSysOpCount(map);
-		
-		//设置真实数据
+
+		// 设置真实数据
 		pageModel.setRows(list);
-		//前台展示的总数数据，json串中也需要有
+		// 前台展示的总数数据，json串中也需要有
 		pageModel.setTotal(total);
-		//展示数据
+		// 展示数据
 		return pageModel;
 	}
-	
+
 	@RequestMapping("selecSysopListPageHelper")
 	@ResponseBody
-	public String selecSysopListPageHelper(String opKind,String opName,int page,int limit) throws Exception{
-		
+	public String selecSysopListPageHelper(String opKind, String opName, int page, int limit) throws Exception {
+
 		Map<String, Object> map = new HashMap<>();
 		map.put("opName", opName);
 		map.put("opKind", opKind);
-		
-		//开始分页
+
+		// 开始分页
 		PageHelper.startPage(page, limit);
-		//分页语句只对最近的有效
+		// 分页语句只对最近的有效
 		List<SysOp> list = userService.selectSysOpListPageHelper(map);
-		//传入要分页的数据
+		// 传入要分页的数据
 		PageInfo<SysOp> pageInfo = new PageInfo<>(list);
-		
+
 		return JSON.toJSONString(pageInfo);
 	}
-	
-	//新增和修改使用同一个页面
+
+	// 新增和修改使用同一个页面
 	@RequestMapping("addUserPage")
-	public String addUserPage(Long opId,Model model) throws Exception{
+	public String addUserPage(Long opId, Model model) throws Exception {
 		SysOp sysOp = new SysOp();
 		String type = "add";
-		//也可以在请求参数里加上type，？的形式追加
+		// 也可以在请求参数里加上type，？的形式追加
 		if (opId != null) {
 			type = "modify";
-			//要修改
+			// 要修改
 			sysOp = userService.selectByPrimaryKey(opId);
 		}
-		model.addAttribute("type",type);
-		model.addAttribute("sysOp",sysOp);
-		//跳转到addUser.jsp的页面
+		model.addAttribute("type", type);
+		model.addAttribute("sysOp", sysOp);
+		// 跳转到addUser.jsp的页面
 		return "user/addUser";
 	}
-	
+
 	@RequestMapping("addUser")
 	@ResponseBody
-	public ResultBean addUser(SysOp sysOp,HttpSession session) throws Exception{
+	public ResultBean addUser(SysOp sysOp, HttpSession session) throws Exception {
 		SysOp currentOp = (SysOp) session.getAttribute("currentUser");
 		ResultBean resultBean = new ResultBean();
-		//保存操作员用户
+		// 保存操作员用户
 		sysOp.setCreator(currentOp.getOpId());
 		int row = userService.saveSysOp(sysOp);
 		if (row == 0) {
 			logger.error("新增用户失败！");
 			resultBean.setResult(ResultBean.RESULT_FAIL);
 			resultBean.setMsg("新增失败！");
-		}else{
+		} else {
 			resultBean.setResult(ResultBean.RESULT_SUCCESS);
 			resultBean.setMsg("新增成功！");
 		}
 		return resultBean;
 	}
-	
+
 	@RequestMapping("modifyUser")
 	@ResponseBody
-	public ResultBean modifyUser(SysOp sysOp) throws Exception{
-		
+	public ResultBean modifyUser(SysOp sysOp) throws Exception {
+
 		ResultBean resultBean = new ResultBean();
-		//修改账户
+		// 修改账户
 		int row = userService.modifySysOp(sysOp);
-		
+
 		if (row > 0) {
 			resultBean.setMsg("修改成功");
 			resultBean.setResult(ResultBean.RESULT_SUCCESS);
-		}else {
+		} else {
 			logger.error("修改失败！");
 			resultBean.setMsg("修改失败");
 			resultBean.setResult(ResultBean.RESULT_FAIL);
 		}
 		return resultBean;
 	}
-	
+
 	/**
 	 * 
-	 * @方法名: upload  
+	 * @方法名: upload
 	 * @功能描述: 静态上传的处理器
 	 * @param uploadFile
 	 * @return
 	 * @throws Exception
-	 * @作者 liuji 
+	 * @作者 liuji
 	 * @日期 2016年12月16日
 	 */
 	@RequestMapping("upload")
 	@ResponseBody
-	public ResultBean upload(MultipartFile uploadFile,HttpSession session)throws Exception{
+	public ResultBean upload(MultipartFile uploadFile, HttpSession session) throws Exception {
 		/*
-		 * ①spring获取多部件的对象（MultipartFile）
-		 * ②准备文件的存储路径
-		 * 		Ⅰ：确认路径，判断是否存在，不存在就创建
-		 * 		Ⅱ：上面的路径+文件的名字，准备好存储路径
-		 * ③spring的transferTo方法，参数为准备好的文件的存储路径
+		 * ①spring获取多部件的对象（MultipartFile） ②准备文件的存储路径 Ⅰ：确认路径，判断是否存在，不存在就创建
+		 * Ⅱ：上面的路径+文件的名字，准备好存储路径 ③spring的transferTo方法，参数为准备好的文件的存储路径
 		 */
 		ResultBean resultBean = new ResultBean();
-		//file目录下，要加\\
+		// file目录下，要加\\
 		String realPath = session.getServletContext().getRealPath("/UserPhoto");
-		String filePath = realPath+"\\";
+		String filePath = realPath + "\\";
 		File file = new File(filePath);
 		if (!file.exists()) {
 			file.mkdirs();
 		}
-		//正真的上传文件
+		// 正真的上传文件
 		uploadFile.transferTo(new File(filePath + uploadFile.getOriginalFilename()));
-		
-		
+
 		resultBean.setResult(ResultBean.RESULT_SUCCESS);
-		resultBean.setMsg("/UserPhoto/"+uploadFile.getOriginalFilename());
+		resultBean.setMsg("/UserPhoto/" + uploadFile.getOriginalFilename());
 		return resultBean;
 	}
-	
-	//处理删除请求
+
+	// 处理删除请求
 	@RequestMapping("deleteUser")
 	@ResponseBody
 	public String deleteUser(Long opId, HttpSession session) throws Exception {
@@ -203,24 +200,24 @@ public class UserConstroller {
 		}
 		return JSON.toJSONString(resultBean, SerializerFeature.WriteMapNullValue);
 	}
-	
+
 	@RequestMapping("selecSysRoleListPageHelper")
 	@ResponseBody
-	public PageInfo<SysRole> selecSysRoleListPageHelper(String roleName,int page,int limit)throws Exception{
-		
+	public PageInfo<SysRole> selecSysRoleListPageHelper(String roleName, int page, int limit) throws Exception {
+
 		Map<String, Object> map = new HashMap<>();
 		map.put("roleName", roleName);
-		//开始分页
+		// 开始分页
 		PageHelper.startPage(page, limit);
 		List<SysRole> list = userService.selecSysRoleListPageHelper(map);
 		PageInfo<SysRole> pageInfo = new PageInfo<>(list);
-		
+
 		return pageInfo;
 	}
-	
+
 	@RequestMapping("selectSysFuncByRoleId")
 	@ResponseBody
-	public List<SysFuncZtreeBean> selectSysFuncByRoleId(Long roleId) throws Exception{
+	public List<SysFuncZtreeBean> selectSysFuncByRoleId(Long roleId) throws Exception {
 		Map<String, Object> map = new HashMap<>();
 		map.put("roleId", roleId);
 		List<SysFuncBean> myRoleList = userService.selectSysFuncByRoleId(map);
@@ -237,12 +234,13 @@ public class UserConstroller {
 		}
 		return list;
 	}
-	
+
 	@RequestMapping("updateRoleFuncRelationship")
 	@ResponseBody
-	public ResultBean updateRoleFuncRelationship(HttpSession session,Long roleId,@RequestParam(value="funcIdList[]",required=false)List<Long> funcIdList) throws Exception{
+	public ResultBean updateRoleFuncRelationship(HttpSession session, Long roleId,
+			@RequestParam(value = "funcIdList[]", required = false) List<Long> funcIdList) throws Exception {
 		ResultBean resultBean = new ResultBean();
-		
+
 		Map<String, Object> map = new HashMap<>();
 		map.put("roleId", roleId);
 		SysOp sysOp = (SysOp) session.getAttribute("currentUser");
@@ -257,15 +255,15 @@ public class UserConstroller {
 				list.add(sysRoleFunc);
 			}
 		}
-		boolean flag = userService.updateRoleFuncRelationship(map,list);
+		boolean flag = userService.updateRoleFuncRelationship(map, list);
 		if (flag) {
 			resultBean.setResult(ResultBean.RESULT_SUCCESS);
 			resultBean.setMsg("修改成功！");
-		}else{
+		} else {
 			resultBean.setResult(ResultBean.RESULT_FAIL);
 			resultBean.setMsg("修改失败！");
 		}
-		
+
 		return resultBean;
 	}
 }
